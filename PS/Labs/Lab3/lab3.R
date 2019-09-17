aa768c269b8c6f95157b19e139cab9f404c72f0d
Shehanka <hcsperera@outlook.com>
2019-09-17 11:30:09 +0530

    Lab 7

--- PS/Labs/Lab3/lab3.R
+++ PS/Labs/Lab3/lab3.R
@@ -3,6 +3,7 @@ library(readxl)
 
 # Q1 - Data set import
 data3 <- read_excel("DATA 3.xlsx")
+names(data3) <- c("Age", "Gender", "Accommodation")
 
 # Data set viewing
 # View(data3)
--- PS/Labs/Lab7/lab7.R
+++ PS/Labs/Lab7/lab7.R
@@ -1,1 +1,11 @@
+# Data import
+Forest<-read.delim("Forest.txt",sep=",")
+
+# Variables
+names(Forest)
+
+# Summery
 summary(Forest)
+
+#No of observations
+nrow(Forest)
