ab76c4625c65aa66e75c4400941307201557fa1b
Shehanka <hcsperera@outlook.com>
2019-09-18 03:04:43 +0530

    Quick sort added

---
+++ DSA/Sample Codes/Sorting/quick_sort.py
@@ -0,0 +1,15 @@
+def partition(array, p, r):
+    x = array[r]
+    i = p - 1
+    for j in range(p, r - 1):
+        if(array[j] <= x):
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[r] = array[r], array[i + 1]
+    return i + 1
+
+def quick_sort(array, p, r):
+    if(p < r):
+        q = partition(array, p, r)
+        quick_sort(array, p, q-1)
+        quick_sort(array, q + 1, r)
