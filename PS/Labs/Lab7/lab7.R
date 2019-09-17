7b08c92053ca3bb9f689ae0a883126f6f396c6cd
Shehanka <hcsperera@outlook.com>
2019-09-17 13:24:29 +0530

    Few more to go

--- PS/Labs/Lab7/lab7.R
+++ PS/Labs/Lab7/lab7.R
@@ -9,3 +9,31 @@ summary(Forest)
 
 #No of observations
 nrow(Forest)
+
+# Max = 100 , min = 15, wind
+max(Forest$wind)
+min(Forest$wind)
+
+# 5 number summery of temp 0.0  6.5  8.4 10.8 56.1
+fivenum(Forest$temp)
+
+# Funtion for outliers
+outval<-boxplot(Forest$wind)$out
+
+# Boxplot
+boxplot(Forest$wind)
+
+# median temp = 8.4
+median(Forest$temp)
+
+# mean = 9.021663 and sd = 4.559477
+mean(Forest$temp)
+sd(Forest$temp)
+
+# IQR = 20
+IQR(Forest$wind)
+
+# How many observations have measured during Friday in August
+my.data<-subset(Forest,(Forest$day=="Friday")&(Forest$month=="August"))
+nrow(my.data)
+
