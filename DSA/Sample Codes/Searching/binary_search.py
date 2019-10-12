2b1bdc8c4330c20927d622e434cb36975b003204
Shehanka <hcsperera@outlook.com>
2019-10-12 17:39:29 +0530

    Binary Search added

---
+++ DSA/Sample Codes/Searching/binary_search.py
@@ -0,0 +1,24 @@
+def binarySearch (arr, l, r, x):
+
+    if r >= l :
+        mid = l + (r - 1) / 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            binarySearch(arr, l, mid - 1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+    else:
+        return -1
+
+arr = [ 2, 3, 4, 10, 40 ] 
+x = 10
+
+# Function call 
+result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print ("Element is present at index " , result)
+else: 
+    print ("Element is not present in array")
