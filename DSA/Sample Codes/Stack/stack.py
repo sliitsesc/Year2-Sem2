2c0fe1396f7304193249c7f20057df7b16d87321
Shehanka <hcsperera@outlook.com>
2019-10-12 23:58:37 +0530

    Stack py added

--- DSA/Sample Codes/Stack/stack.py
+++ DSA/Sample Codes/Stack/stack.py
@@ -4,3 +4,13 @@ top = -1
 
 def push(value):
     stack_array.append(value)
+    top += 1 
+
+def pop():
+    top -= 1
+    value = stack_array.pop()
+    return value
+
+def peek():
+    return stack_array[stack_array.length -1]
+
