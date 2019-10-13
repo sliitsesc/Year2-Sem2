d9ec83bc518e3a7425d70deed34c0b3c841deb38
Shehanka <hcsperera@outlook.com>
2019-10-14 02:49:19 +0530

    Circular Queue Added

--- DSA/Sample Codes/Queue/CircularQueue.java
+++ DSA/Sample Codes/Queue/CircularQueue.java
@@ -1,39 +1,40 @@
-/**
- * @author chamodshehanka on 8/6/2019
- * @project Demo
- **/
-public class Queue {
-
+public class CircularQueue {
     private int maxSize;
     private int[] queueArray;
     private int front;
     private int rear;
     private int nItems;
 
-    public Queue(int maxSize) {
+    public CircularQueue(int maxSize) {
         this.maxSize = maxSize;
         queueArray = new int[maxSize];
         front = 0;
         rear = -1;
-        nItems = 0; //Queue is empty
+        nItems = 0;
     }
 
-    public void insert(int val){
-        if (rear == maxSize -1){
-            System.out.println("Queue is full");
-        }else {
+    public void insert(int val) {
+        if(nItems == maxSize) {
+            System.out.println("Queue is full !!");
+        } else {
+            if(rear == maxSize -1) {
+                rear--;
+            }
             queueArray[++rear] = val;
             nItems++;
         }
     }
 
-    public int remove(){
+    public int remove() {
         if (nItems == 0){
             System.out.println("Queue is empty");
             return -99;
         }else {
+            int num = queueArray[front++];
+            if(front == maxSize)
+                front = 0;
             nItems--;
-            return queueArray[front++];
+            return num;
         }
     }
 
