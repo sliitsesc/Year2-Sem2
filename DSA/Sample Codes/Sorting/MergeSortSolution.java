8db88f709a605104bbd5f9dc095ac436fc7de4c4
Shehanka <hcsperera@outlook.com>
2019-10-21 18:31:20 +0530

    Merge sort java added

---
+++ DSA/Sample Codes/Sorting/MergeSortSolution.java
@@ -0,0 +1,52 @@
+/**
+ * @author chamodshehanka on 10/21/2019
+ * @project Demo
+ **/
+public class MergeSortSolution {
+
+    public static void mergeSort(int[] array) {
+        mergeSort(array, new int[array.length], 0, array.length - 1);
+    }
+
+    public static void mergeSort(int[] array, int[] temp, int leftStart, int rightEnd) {
+        if (leftStart >= rightEnd) {
+            return;
+        }
+        int middle = (leftStart + rightEnd) / 2;
+
+        mergeSort(array, temp, leftStart, middle);
+        mergeSort(array, temp, middle + 1, rightEnd);
+        mergeHalves(array, temp, leftStart, rightEnd);
+    }
+
+    private static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd) {
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightEnd - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (array[left] <= array[right]) {
+                temp[index] = array[left];
+                left++;
+            } else {
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
+        }
+
+        System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(array, left, temp, index, rightEnd - right + 1);
+        System.arraycopy(temp, leftStart, array, leftStart, size);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
