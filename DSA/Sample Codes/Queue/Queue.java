b1e6ef70164216b97ded50c7473480773b43d2fa
Shehanka <hcsperera@outlook.com>
2019-10-22 18:22:17 +0530

    Typos

--- DSA/Sample Codes/Queue/Queue.java
+++ DSA/Sample Codes/Queue/Queue.java
@@ -18,30 +18,30 @@ public class Queue {
         nItems = 0; //Queue is empty
     }
 
-    public void insert(int val){
-        if (rear == maxSize -1){
+    public void insert(int val) {
+        if (rear == maxSize -1) {
             System.out.println("Queue is full");
-        }else {
+        } else {
             queueArray[++rear] = val;
             nItems++;
         }
     }
 
-    public int remove(){
-        if (nItems == 0){
+    public int remove() {
+        if (nItems == 0) {
             System.out.println("Queue is empty");
             return -99;
-        }else {
+        } else {
             nItems--;
             return queueArray[front++];
         }
     }
 
-    public int peekFront(){
-        if (nItems == 0){
+    public int peekFront() {
+        if (nItems == 0) {
             System.out.println("Queue is empty");
             return -99;
-        }else {
+        } else {
             return queueArray[front];
         }
     }
