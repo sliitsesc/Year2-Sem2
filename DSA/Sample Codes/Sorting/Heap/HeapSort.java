c612c1e5a0903b98efa268d08385f9e3473cb237
Shehanka <hcsperera@outlook.com>
2019-10-22 22:24:54 +0530

    Heap sort

---
+++ DSA/Sample Codes/Sorting/Heap/HeapSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * @author chamodshehanka on 10/22/2019
+ * @project Demo
+ **/
+
+
+public class HeapSort {
+    private int capacity = 10;
+    private int size = 0;
+
+    private int[] items = new int[capacity];
+
+    private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
+    private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
+    private int getParentIndex(int childIndex) { return (childIndex -1) / 2; }
+
+    private boolean hasLeftChild(int index) { return getLeftChildIndex(index) < size; }
+    private boolean hasRightChild(int index) { return getRightChildIndex(index) < size; }
+    private boolean hasParent(int index) { return getParentIndex(index) > 0; }
+
+    private int leftChild(int index) { return items[getLeftChildIndex(index)]; }
+    private int rightChild(int index) { return items[getRightChildIndex(index)]; }
+    private int parent(int index) { return items[getParentIndex(index)]; }
+
+    private void swap(int indexOne, int indexTwo) {
+        int temp = items[indexOne];
+        items[indexOne] = items[indexTwo];
+        items[indexTwo] = items[indexTwo];
+        items[indexTwo] = temp;
+    }
+
+    private void ensureExtraCapacity() {
+        if (size == capacity) {
+            items = Arrays.copyOf(items,capacity * 2);
+            capacity *= 2;
+        }
+    }
+}
